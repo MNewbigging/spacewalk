5e23a52fdde79f35b3582de28f016fd763091163
MNewbigging <mrnewbigging@gmail.com>
2021-10-12 17:44:35 +0100

    added starry background

--- src/components/game-screen/GameScreen.tsx
+++ src/components/game-screen/GameScreen.tsx
@@ -6,6 +6,7 @@ import { FallingObject } from './game-objects/FallingObject';
 import { LetterObject } from './game-objects/LetterObject';
 
 import './game-screen.scss';
+import './game-background.scss';
 
 interface Props {
   gameState: GameState;
@@ -18,6 +19,17 @@ export class GameScreen extends React.Component<Props> {
 
     return (
       <div className={'game-screen'}>
+        <div className={'background'}>
+          <div className={'stars-small'}></div>
+          <div className={'stars-small red'}></div>
+          <div className={'stars-small blue'}></div>
+          <div className={'stars-med'}></div>
+          <div className={'stars-med red'}></div>
+          <div className={'stars-med blue'}></div>
+          <div className={'stars-large'}></div>
+          <div className={'stars-large red'}></div>
+          <div className={'stars-large blue'}></div>
+        </div>
         {gameState.letterObjects.map((obj) => (
           <FallingObject key={obj.id} fallingObject={obj}>
             <LetterObject letterObject={obj} />
