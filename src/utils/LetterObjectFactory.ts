d5677a6cb1238dd010273b185fe664a65804a8a1
MNewbigging <mrnewbigging@gmail.com>
2021-10-14 11:10:48 +0100

    weighted letters

--- src/utils/LetterObjectFactory.ts
+++ src/utils/LetterObjectFactory.ts
@@ -2,7 +2,18 @@ import { CSSProperties } from 'react';
 import { LetterObjectState } from '../state/LetterObjectState';
 import { RandomUtils } from './RandomUtils';
 
+enum Letters {
+  COMMON = 'srntlcdgmphaeiou', // 3
+  UNCOMMON = 'yfvb', // 2
+  RARE = 'kwzjqz', // 1
+}
+
 export class LetterObjectFactory {
+  private static commonWeight = 3;
+  private static uncommonWeight = 2;
+  private static rareWeight = 1;
+  private static lettersString = LetterObjectFactory.makeLettersString();
+
   public static createLetterObject() {
     // Position along screen using left property
     const leftPos = RandomUtils.getRandomRangeInt(1, 80);
@@ -17,8 +28,36 @@ export class LetterObjectFactory {
     const maxLetters = 10;
     const letterCount = RandomUtils.getRandomRangeInt(minLetters, maxLetters);
 
-    const letters = RandomUtils.getRandomLetters(letterCount);
+    const letters = this.pickLetters(this.lettersString, letterCount);
 
     return new LetterObjectState(style, letters);
+  }
+
+  private static makeLettersString() {
+    let letters = '';
+
+    for (let i = 0; i < this.commonWeight; i++) {
+      letters += Letters.COMMON;
+    }
+
+    for (let i = 0; i < this.uncommonWeight; i++) {
+      letters += Letters.UNCOMMON;
+    }
+
+    for (let i = 0; i < this.rareWeight; i++) {
+      letters += Letters.RARE;
+    }
+
+    return letters;
+  }
+
+  private static pickLetters(lettersString: string, letterCount: number) {
+    let picked = '';
+
+    for (let i = 0; i < letterCount; i++) {
+      picked += lettersString.charAt(Math.floor(Math.random() * lettersString.length));
+    }
+
+    return picked;
   }
 }
