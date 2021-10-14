1d08e2bd4c8e3a471bdd5406c4971fca0aa398c4
MNewbigging <mrnewbigging@gmail.com>
2021-10-14 11:16:31 +0100

    moved game background to app level

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,15 +6,35 @@ import { StartScreen } from './start-screen/StartScreen';
 import { GameScreen } from './game-screen/GameScreen';
 
 import './app.scss';
+import './game-screen/game-background.scss';
 
 @observer
 export class App extends React.PureComponent {
   private readonly appState = new AppState();
   public render() {
+    let screen: JSX.Element = undefined;
+
     if (this.appState.screen === Screen.GAME && this.appState.gameState) {
-      return <GameScreen gameState={this.appState.gameState} />;
+      screen = <GameScreen gameState={this.appState.gameState} />;
+    } else {
+      screen = <StartScreen startGame={this.appState.startGame} />;
     }
 
-    return <StartScreen startGame={this.appState.startGame} />;
+    return (
+      <>
+        <div className={'background'}>
+          <div className={'stars-small'}></div>
+          <div className={'stars-small red'}></div>
+          <div className={'stars-small blue'}></div>
+          <div className={'stars-med'}></div>
+          <div className={'stars-med red'}></div>
+          <div className={'stars-med blue'}></div>
+          <div className={'stars-large'}></div>
+          <div className={'stars-large red'}></div>
+          <div className={'stars-large blue'}></div>
+          {screen}
+        </div>
+      </>
+    );
   }
 }
